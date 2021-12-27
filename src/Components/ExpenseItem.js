2b81afafe5e7c9d704686b88cf8cd2e65336ef69
mertmutlu <mertmutlu33@gmail.com>
2021-12-27 11:07:19 +0300

    init()

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,13 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate= new Date(2021,10,28);
-  const expenseTitle='Car Insurance';
-  const expensePrice=350;
-    return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expensePrice}</div>
-      </div>    
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 }
